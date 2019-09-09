d4d2b574a5f7a9b0f195a78359e855361ab46f6c
aleksivaisanen <aleksi.vaisanen@elisanet.fi>
2019-09-09 21:12:03 +0300

    added secret message

--- src/App.js
+++ src/App.js
@@ -17,6 +17,21 @@ class App extends React.Component {
   }
 
   componentDidMount() {
+    var styles = [
+      'background: linear-gradient(#D33106, #571402)'
+      , 'border: 1px solid #3E0E02'
+      , 'color: white'
+      , 'display: block'
+      , 'font-size: 32px'
+      , 'text-shadow: 0 1px 0 rgba(0, 0, 0, 0.3)'
+      , 'box-shadow: 0 1px 0 rgba(255, 255, 255, 0.4) inset, 0 5px 3px -5px rgba(0, 0, 0, 0.5), 0 -13px 5px -10px rgba(255, 255, 255, 0.4) inset'
+      , 'line-height: 40px'
+      , 'text-align: center'
+      , 'font-weight: bold'
+    ].join(';');
+
+    console.log("%c TUPLIIIIIIIIIS! ", styles)
+
     this.interval = setInterval(() => {
       this.leftToCruise()
     }, 1000);
